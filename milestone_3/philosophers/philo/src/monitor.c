87076b9c2d4667ff821c5422b5574b3e3023708c
Mário <marioandrerodrigues@hotmail.com>
2026-08-19 19:46:33 +0100

    nothing

---
+++ milestone_3/philosophers/philo/src/monitor.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitor.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/03 01:25:18 by mario             #+#    #+#             */
+/*   Updated: 2026/04/10 13:56:11 by mario            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philosophers.h"
+
+static int	check_fullness(t_data *data)
+{
+	int	i;
+	int	full_count;
+
+	i = 1;
+	full_count = 0;
+	while (i <= data->params.num_of_philos)
+	{
+		pthread_mutex_lock(&data->mutex.meal_lock);
+		if (data->philos[i].full == 1)
+			full_count++;
+		pthread_mutex_unlock(&data->mutex.meal_lock);
+		if (full_count == data->params.num_of_philos)
+		{
+			pthread_mutex_lock(&data->mutex.dead_lock);
+			data->params.dead_flag = 1;
+			pthread_mutex_unlock(&data->mutex.dead_lock);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+static int	check_death(t_data *data)
+{
+	int		i;
+	long	starve_time;
+	int		is_full;
+
+	i = 1;
+	while (i <= data->params.num_of_philos)
+	{
+		pthread_mutex_lock(&data->mutex.meal_lock);
+		starve_time = ft_get_time() - data->philos[i].last_meal;
+		is_full = data->philos[i].full;
+		pthread_mutex_unlock(&data->mutex.meal_lock);
+		if (starve_time >= data->params.time_to_die && !is_full)
+		{
+			pthread_mutex_lock(&data->mutex.dead_lock);
+			data->params.dead_flag = 1;
+			pthread_mutex_unlock(&data->mutex.dead_lock);
+			pthread_mutex_lock(&data->mutex.print_lock);
+			printf("%ld %d died\n", ft_get_time() - data->params.time_start,
+				data->philos[i].id);
+			pthread_mutex_unlock(&data->mutex.print_lock);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+void	monitor(t_data *data)
+{
+	while (1)
+	{
+		if (!check_death(data) || !check_fullness(data))
+			break ;
+		usleep(500);
+	}
+}
---
+++ milestone_3/philosophers/philo/src/parser.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/05 21:38:58 by mario             #+#    #+#             */
+/*   Updated: 2026/04/10 13:55:56 by mario            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+long	ft_atol(const char *str)
+{
+	long	num;
+	int		i;
+
+	num = 0;
+	i = 0;
+	if (!str || str[0] == '\0')
+		return (-1);
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (-1);
+		if (num > (LONG_MAX - (str[i] - '0')) / 10)
+			return (-1);
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	return (num);
+}
+
+static int	validate_counts(long n_philos, long max_meals, int ac)
+{
+	if (n_philos < 1)
+	{
+		printf("Number of philosophers must be greater than 0\n");
+		return (1);
+	}
+	if (ac == 6 && max_meals <= 0)
+	{
+		printf("Max meals must be greater than 0\n");
+		return (1);
+	}
+	return (0);
+}
+
+static int	validate_times(long t_die, long t_eat, long t_sleep)
+{
+	if (t_die <= 0 || t_eat <= 0 || t_sleep <= 0)
+	{
+		printf("Time values must be greater than 0\n");
+		return (1);
+	}
+	return (0);
+}
+
+static int	parse_values(char **av, int ac, long *out)
+{
+	out[0] = ft_atol(av[1]);
+	out[1] = ft_atol(av[2]);
+	out[2] = ft_atol(av[3]);
+	out[3] = ft_atol(av[4]);
+	if (ac == 6)
+		out[4] = ft_atol(av[5]);
+	else
+		out[4] = -1;
+	if (out[0] == -1 || out[1] == -1 || out[2] == -1 || out[3] == -1
+		|| (ac == 6 && out[4] == -1))
+	{
+		printf("Usage: ./philosophers n t_die t_eat t_sleep [max]\n");
+		return (1);
+	}
+	return (0);
+}
+
+int	parse_args(int ac, char **av)
+{
+	long	args[5];
+
+	if (ac != 5 && ac != 6)
+	{
+		printf("Usage: ./philosophers n t_die t_eat t_sleep [max]\n");
+		return (1);
+	}
+	if (parse_values(av, ac, args))
+		return (1);
+	if (validate_counts(args[0], args[4], ac))
+		return (1);
+	if (validate_times(args[1], args[2], args[3]))
+		return (1);
+	return (0);
+}
