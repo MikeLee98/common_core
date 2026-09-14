8d959245fec1ed35e9df27e254556428a93fd8ac
Mario Rodrigues <marioro2@c2r2s8.42lisboa.com>
2026-09-14 17:35:36 +0100

    nothing

--- milestone_4/cpp/cpp01/ex04/main.cpp
+++ milestone_4/cpp/cpp01/ex04/main.cpp
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Mario <Mario@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marioro2 <marioro2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/10 12:43:22 by marioro2          #+#    #+#             */
-/*   Updated: 2026/09/14 00:04:21 by Mario            ###   ########.fr       */
+/*   Updated: 2026/09/14 17:35:21 by marioro2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
-#include <iostream>
 #include <fstream>
 
 int main(int argc, char **argv)
@@ -21,18 +20,27 @@ int main(int argc, char **argv)
 	pos = 0;
 	if (argc != 4)
 		return (1);
+	if (std::string(argv[2]).empty())
+		return (1);
 	std::ifstream inputFile(argv[1]);
 	if (!inputFile)
 		return (1);
 	std::istreambuf_iterator<char> begin(inputFile);
 	std::istreambuf_iterator<char> end;
 	std::string content(begin, end);
-	std::ofstream outputFile(std::string(argv[1]) + ".replace");
+	std::string outputName = std::string(argv[1]) + ".replace";
+	std::ofstream outputFile(outputName.c_str());
 	if (!outputFile)
 		return (1);
 	pos = content.find(argv[2]);
 	while (pos != std::string::npos)
 	{
-		
+		std::string before = content.substr(0, pos);
+		before += argv[3];
+		before += content.substr(pos + std::string(argv[2]).size());
+		content = before;
+		pos = content.find(argv[2], pos + std::string(argv[3]).size());
 	}
+	outputFile << content;
+	return (0);
 }
