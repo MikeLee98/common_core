3d4ac40e45e6ff4e5e7803d001e63171ff7b32cf
Mario Rodrigues <marioro2@c2r2s4.42lisboa.com>
2026-09-16 17:42:51 +0100

    nothing

--- milestone_4/cpp/cpp02/ex00/Fixed.cpp
+++ milestone_4/cpp/cpp02/ex00/Fixed.cpp
@@ -6,8 +6,24 @@
 /*   By: marioro2 <marioro2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/15 14:13:19 by marioro2          #+#    #+#             */
-/*   Updated: 2026/09/15 14:13:29 by marioro2         ###   ########.fr       */
+/*   Updated: 2026/09/16 17:27:27 by marioro2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
 
+Fixed::Fixed()
+{
+	_value = 0;
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+	_value = other._value;
+}
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+	_value = other._value;
+	return (*this);
+}
--- milestone_4/cpp/cpp02/ex00/Fixed.hpp
+++ milestone_4/cpp/cpp02/ex00/Fixed.hpp
@@ -6,8 +6,26 @@
 /*   By: marioro2 <marioro2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/15 14:13:02 by marioro2          #+#    #+#             */
-/*   Updated: 2026/09/15 14:13:04 by marioro2         ###   ########.fr       */
+/*   Updated: 2026/09/16 15:59:55 by marioro2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+#define FIXED_HPP
 
+class Fixed
+{
+	private:
+		int _value;
+    	static const int _fractionalBits = 8;
+
+	public:
+		Fixed();
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
+#endif
