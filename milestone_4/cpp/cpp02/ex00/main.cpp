844a6faec8e57524460b04139a55d2758312e083
Mario <marioandrerodrigueshotmail.com>
2026-09-18 15:50:05 +0100

    nothing

--- milestone_4/cpp/cpp02/ex00/main.cpp
+++ milestone_4/cpp/cpp02/ex00/main.cpp
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marioro2 <marioro2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Mario <Mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/15 14:12:49 by marioro2          #+#    #+#             */
-/*   Updated: 2026/09/17 15:39:53 by marioro2         ###   ########.fr       */
+/*   Updated: 2026/09/18 15:46:11 by Mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 
-int main( void ) 
+int main(void) 
 {
 	Fixed a;
 	Fixed b( a );
--- milestone_4/cpp/cpp02/ex03/main.cpp
+++ milestone_4/cpp/cpp02/ex03/main.cpp
@@ -6,28 +6,8 @@
 /*   By: Mario <Mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/15 14:12:49 by marioro2          #+#    #+#             */
-/*   Updated: 2026/09/18 15:46:39 by Mario            ###   ########.fr       */
+/*   Updated: 2026/09/18 15:49:59 by Mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
-#include <iostream>
-
-int main(void)
-{
-	Fixed a;
-	Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	Fixed const d( b );
-
-	a = Fixed( 1234.4321f );
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	return (0);
-}
